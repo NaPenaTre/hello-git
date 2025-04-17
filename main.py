34a949563156bb92992ed020f0308ace2ac724be
Nayita <nayita@pycharm.com>
2025-04-16 22:13:14 -0400

    Prueba
    agregando el import para isort y pre commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
 
 
 def print_hi(name):
@@ -13,4 +14,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+print(math.isclose(0,0))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
